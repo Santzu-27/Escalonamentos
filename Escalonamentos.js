0611b10909a462c010912f87f932488057f10350
Gabriel <gabi260402@gmail.com>
2024-03-20 10:55:08 -0300

    Update Escalonamentos.js

--- Escalonamentos.js
+++ Escalonamentos.js
@@ -4,7 +4,7 @@ function rand(max){
     return num;
 }
 
-class Processos{
+class Processo{
     constructor(tempo_chegada, tempo_execucao, prioridade){
         this.tempo_chegada = tempo_chegada,
         this.tempo_execucao = tempo_execucao,
@@ -12,15 +12,18 @@ class Processos{
         this.tempo_restante = this.tempo_execucao;
     }
 }
-
+const processos = [];
 function populaAleatorio(){
-    
+    const p1 = new Processo(rand(30), rand(30), rand(10));   
+    const p2 = new Processo(rand(30), rand(30), rand(10));
+    const p3 = new Processo(rand(30), rand(30), rand(10));
+    processos.splice(0, processos.length)
+    processos.push(p1);
+    processos.push(p2);
+    processos.push(p3);
+    console.log (processos)
 }
 
 function populaManual(){
 
 }
-
-const p1 = new Processos(2, 10, 3, 0);
-
-console.log(p1)
