f060443349a8ea9b3054020101fd136ea6e9e728
Gabriel <gabi260402@gmail.com>
2024-03-20 15:06:50 -0300

    fcfs

--- Escalonamentos.js
+++ Escalonamentos.js
@@ -1,5 +1,6 @@
 const mainPage = document.getElementById('main')
 const divInserir = document.getElementById('inserir')
+const divTempos = document.getElementById('tempos')
 function rand(max) {
     const min = 0;
     let num = Math.floor(Math.random() * (max - min) + 1)
@@ -25,11 +26,12 @@ function populaAleatorio() {
     processos.push(p2);
     processos.push(p3);
     console.log(processos)
+    mostraProcessos();
 }
 
 function populaManual(i) {
-    if(i > 3) {
-        // mostraProcessos();
+    if(i > 2) {
+        mostraProcessos();
         console.log(processos);
         return;
     }
@@ -58,4 +60,38 @@ function addManual(i) {
         prioridade: Number(prior.value)
     })
     populaManual(i);
+}
+
+function mostraProcessos() {
+    document.getElementById('inicio').style.display = 'none';
+    divInserir.innerHTML = '';
+    let i = 0;
+    for(info of processos){
+        divInserir.innerHTML += `
+        <div id="processos">
+                <h1>Processo ${i}</h1>
+                <p>Tempo de Chegada: ${info.tempo_chegada}</p>
+                <p>Tempo de Execução: ${info.tempo_execucao}</p>
+                <p>Prioridade: ${info.prioridade}</p>
+                </div>
+        `
+        i++;
+    }
+    document.getElementById('comandos').style.display = 'flex';
+}
+
+function fcfs(){
+    let tempo = 0;
+    let i= 0;
+    let processoAtual = processos[i];
+    divTempos.style.display = 'block';
+    while(processoAtual.tempo_restante >= 0){
+        tempo++;
+        let novoTempo = document.createElement('div');
+        novoTempo.innerHTML = `
+            <div>Tempo[${tempo}]: Processo[${i}] restante = ${processoAtual.tempo_restante}</div>
+        `
+        processoAtual.tempo_restante--;
+        divTempos.appendChild(novoTempo);
+    }
 }
