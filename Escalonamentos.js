19dbacf3f1d145ecd7d2484302b3fd63c5831189
Gabriel <gabi260402@gmail.com>
2024-03-20 16:39:50 -0300

    Estatisticas e Manual

--- Escalonamentos.js
+++ Escalonamentos.js
@@ -1,6 +1,7 @@
 const mainPage = document.getElementById('main')
 const divInserir = document.getElementById('inserir')
 const divTempos = document.getElementById('tempos')
+
 function rand(max) {
     const min = 0;
     let num = Math.floor(Math.random() * (max - min) + 1)
@@ -13,14 +14,16 @@ class Processo {
             this.tempo_execucao = tempo_execucao,
             this.prioridade = prioridade,
             this.tempo_restante = this.tempo_execucao;
+            this.tempo_espera = 0;
     }
 }
+
 const processos = [];
 function populaAleatorio() {
     divInserir.innerHTML = ``
-    const p1 = new Processo(rand(30), rand(30), rand(10));
-    const p2 = new Processo(rand(30), rand(30), rand(10));
-    const p3 = new Processo(rand(30), rand(30), rand(10));
+    const p1 = new Processo(rand(15), rand(15), rand(5));
+    const p2 = new Processo(rand(15), rand(15), rand(5));
+    const p3 = new Processo(rand(15), rand(15), rand(5));
     processos.splice(0, processos.length)
     processos.push(p1);
     processos.push(p2);
@@ -30,12 +33,14 @@ function populaAleatorio() {
 }
 
 function populaManual(i) {
+    if(i == 0){
+        processos.splice(0, processos.length)
+    }
     if(i > 2) {
         mostraProcessos();
         console.log(processos);
         return;
     }
-
     divInserir.innerHTML = `
         <p>Digite o tempo de execução do Processo ${i}:</p>
         <input type="number" id="exec">
@@ -43,22 +48,19 @@ function populaManual(i) {
         <input type="number" id="cheg">
         <p>Digite a prioridade do Processo ${i}:</p>
         <input type="number" id="prior"> <br>
-        <button id="enviar" onclick="addManual(${i+1})">Enviar</button>
+        <button id="enviar" onclick="addManual(${i})">Enviar</button>
     `
-    
     console.log(i)
-    // populaManual(i)
 }
 
 function addManual(i) {
-    const exec = document.getElementById('exec');
-    const cheg = document.getElementById('cheg');
-    const prior = document.getElementById('prior');
-    processos.push({
-        tempo_chegada: Number(cheg.value),
-        tempo_execucao: Number(exec.value),
-        prioridade: Number(prior.value)
-    })
+    const exec = Number(document.getElementById('exec').value)
+    const cheg = Number(document.getElementById('cheg').value)
+    const prior = Number(document.getElementById('prior').value)
+
+    const processo = new Processo(cheg, exec, prior);
+    processos.push(processo)
+    i++;
     populaManual(i);
 }
 
@@ -82,16 +84,55 @@ function mostraProcessos() {
 
 function fcfs(){
     let tempo = 0;
-    let i= 0;
-    let processoAtual = processos[i];
+    let i= 0; // i = Posição do array de processos
+    divTempos.innerHTML = '';
     divTempos.style.display = 'block';
-    while(processoAtual.tempo_restante >= 0){
+    while(i < processos.length){
+        let processoEmExecucao = processos[i];
+        //Dar tempo de espera verificando se o processo está iniciando
+        if(processoEmExecucao.tempo_execucao == processoEmExecucao.tempo_restante){
+            processoEmExecucao.tempo_espera = tempo;     
+        }
+
         tempo++;
+        processoEmExecucao.tempo_restante--;
+
         let novoTempo = document.createElement('div');
         novoTempo.innerHTML = `
-            <div>Tempo[${tempo}]: Processo[${i}] restante = ${processoAtual.tempo_restante}</div>
+            Tempo[${tempo}]: Processo[${i}] restante = ${processoEmExecucao.tempo_restante}
         `
-        processoAtual.tempo_restante--;
         divTempos.appendChild(novoTempo);
+        if(processoEmExecucao.tempo_restante == 0){
+            processoEmExecucao.tempo_restante = processoEmExecucao.tempo_execucao //Para resetar o processo;
+            i++
+        }
     }
+}
+
+const divStats = document.createElement('div');
+function imprimeStats(){
+    let tempoTotal = 0;
+    let tempoMedio = 0;
+    for(p of processos){
+        tempoTotal += p.tempo_espera;
+    }
+    tempoMedio = tempoTotal / processos.length;
+    divStats.innerHTML = `
+        Tempos de espera: <br>
+        Processo 0 = ${processos[0].tempo_espera}<br>
+        Processo 1 = ${processos[1].tempo_espera}<br>
+        Processo 2 = ${processos[2].tempo_espera}<br>
+        Tempo médio: ${tempoMedio}<br>
+        Tempo total: ${tempoTotal}<br>
+    `
+    divStats.setAttribute('id', 'stats')
+    divTempos.after(divStats)
+}
+
+function popularNovamente(){
+    document.getElementById('inicio').style.display = 'block';
+    divInserir.innerHTML = '';
+    divTempos.style.display = 'none';
+    divStats.innerHTML = '';
+    document.getElementById('comandos').style.display = 'none'
 }
